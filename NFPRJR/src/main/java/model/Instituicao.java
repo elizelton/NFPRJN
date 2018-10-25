6a8d780ae6b1aa13c92221d679fd9f92912a68ed
Gabriel <Gabriel@200.39.72.46>
2018-10-24 23:20:38 -0200

    // Alterada a modelagem

---
+++ NFPRJR/src/main/java/model/Instituicao.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package model;
+
+import java.util.Objects;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+/**
+ * 
+ * @author Gabriel Strack
+ */
+@Document
+public class Instituicao {
+    @Id
+    private String id;
+    private String nome;
+    private String cnpj;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 61 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Instituicao other = (Instituicao) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return nome;
+    }
+    
+    
+}
---
+++ NFPRJR/src/main/java/model/Meses.java
@@ -0,0 +1,113 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.util.Objects;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.CompoundIndex;
+import org.springframework.data.mongodb.core.index.CompoundIndexes;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+/**
+ *
+ * @author Gabriel Strack
+ */
+@Document
+@CompoundIndexes({
+    @CompoundIndex(
+            name = "anoMes_idx",
+            def = "{'ano': 1,'mes': 1}", unique = true)
+
+})
+public class Meses {
+
+    @Id
+    private String id;
+    private String mes;
+    private String ano;
+    private int numeroNotas;
+    private Double valorTotal;
+    private Double credito;
+
+    public String getMes() {
+        return mes;
+    }
+
+    public void setMes(String mes) {
+        this.mes = mes;
+    }
+
+    public String getAno() {
+        return ano;
+    }
+
+    public void setAno(String ano) {
+        this.ano = ano;
+    }
+
+    public int getNumeroNotas() {
+        return numeroNotas;
+    }
+
+    public void setNumeroNotas(int numeroNotas) {
+        this.numeroNotas = numeroNotas;
+    }
+
+    public Double getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(Double valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public Double getCredito() {
+        return credito;
+    }
+
+    public void setCredito(Double credito) {
+        this.credito = credito;
+    }
+    
+    public Double getPercCredito(){
+        return (credito/valorTotal)*100;
+    }
+    
+    public Double getMediaPorNota(){
+        return credito/numeroNotas;
+    }
+    
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 59 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Meses other = (Meses) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+    
+    @Override
+    public String toString() {
+        return mes + "/" + ano + '}';
+    }
+
+}
